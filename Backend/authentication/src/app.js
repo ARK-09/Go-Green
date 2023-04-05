2964f303e941046ae057a61d81848c263cdd6b60
ark-09 <kamransiddique09@gmail.com>
2023-04-05 10:41:19 +0500

    Finished configuring ingress

--- Backend/authentication/src/app.js
+++ Backend/authentication/src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const AppError = require("./util/appError");
 const globalErrorController = require("./controllers/globalErrorController");
+const userRouter= require("./routes/userRouter");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/users");
+app.use("/api/v1/users", userRouter);
 
 app.use("*", (req, res, next) => {
   next(new AppError(`Can't find path: ${req.path} on this server`, 404));
--- Backend/authentication/src/server.js
+++ Backend/authentication/src/server.js
@@ -6,10 +6,13 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 
-mongoose.connect ()
+// mongoose.connect ()
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+const server = app.listen(port, () => {
+  var host = server.address().address
+  var port = server.address().port
+  
+  console.log(`Listening at http://${host}:${port}`)
 });
