9cdee06efacfccd956ad09c9b82a6954ca311bc6
ark-09 <kamransiddique09@gmail.com>
2023-06-21 08:50:02 +0500

    Started Implementing frontend

--- Frontend/app/src/main/java/com/arkindustries/gogreen/CreateJob.kt
+++ Frontend/app/src/main/java/com/arkindustries/gogreen/CreateJob.kt
@@ -3,16 +3,17 @@ package com.arkindustries.gogreen
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.arkindustries.gogreen.databinding.ActivityCreateJobBinding
 import com.arkindustries.gogreen.databinding.ActivityViewJobBinding
 
 
-class ViewJob : AppCompatActivity() {
-    private lateinit var viewJobBinding: ActivityViewJobBinding
+class CreateJob : AppCompatActivity() {
+    private lateinit var createJobBinding: ActivityCreateJobBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewJobBinding = ActivityViewJobBinding.inflate(layoutInflater)
-        setContentView(viewJobBinding.root)
+        createJobBinding = ActivityCreateJobBinding.inflate(layoutInflater)
+        setContentView(createJobBinding.root)
 
 //        ArrayAdapter.createFromResource(
 //            this,
@@ -20,7 +21,7 @@ class ViewJob : AppCompatActivity() {
 //            android.R.layout.simple_spinner_item
 //        ).also { adapter ->
 //            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            viewJobBinding.deadlineSpinner.adapter = adapter
+//            createJobBinding.deadlineSpinner.adapter = adapter
 //        }
     }
 }
