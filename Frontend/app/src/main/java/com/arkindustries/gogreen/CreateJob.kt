eeba353c7b11ae4a306de8cc02649e3cde7c831e
ark-09 <kamransiddique09@gmail.com>
2023-06-21 08:49:00 +0500

    Started Implementing frontend

---
+++ Frontend/app/src/main/java/com/arkindustries/gogreen/CreateJob.kt
@@ -0,0 +1,26 @@
+package com.arkindustries.gogreen
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.arkindustries.gogreen.databinding.ActivityViewJobBinding
+
+
+class ViewJob : AppCompatActivity() {
+    private lateinit var viewJobBinding: ActivityViewJobBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewJobBinding = ActivityViewJobBinding.inflate(layoutInflater)
+        setContentView(viewJobBinding.root)
+
+//        ArrayAdapter.createFromResource(
+//            this,
+//            R.array.project_timeline,
+//            android.R.layout.simple_spinner_item
+//        ).also { adapter ->
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//            viewJobBinding.deadlineSpinner.adapter = adapter
+//        }
+    }
+}
---
+++ Frontend/app/src/main/java/com/arkindustries/gogreen/components/Checkbox.kt
@@ -0,0 +1,4 @@
+package com.arkindustries.gogreen.components
+
+class Checkbox {
+}
